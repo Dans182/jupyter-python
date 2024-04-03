1ce242b1ffe4170d5bdf93c7e605b3545eaaabd5
Daniel Gaiteiro <dgaiteiro@gmail.com>
2024-04-03 16:26:27 +0200

    update dia 3

---
+++ Daniel/matematicas.py
@@ -0,0 +1,10 @@
+pi = 3.141592
+
+def suma(a, b):
+    """Suma los valores a y b, pasados como argumentos"""
+    #a lo de arriba se le llaman docstring. Son ayudas de la documentación
+    return a + b
+
+def multiplica(a, b):
+    """Multiplica los valores a y b, pasados como argumentos"""
+    return a * b
